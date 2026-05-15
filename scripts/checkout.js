89bb5385b30a4b8e12b316923294d8ba6f119658
Ketan Kashikar <ketan07kashikar@gmail.com>
2026-05-16 02:17:21 +0530

    amazonclone commit

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
@@ -106,7 +106,9 @@ function deliveryOptionHTML(matchingProduct, cartItem){
       cartItem.deliveryOptionId;
 
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
         ${isChecked ?'checked' :''}
           class="delivery-option-input"
@@ -140,5 +142,13 @@ document.querySelectorAll('.js-delete-link')
     });
   });
 
+  document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    });
+  });
+
   
-  
+   
